d0e207ad5e77ad1c78fd6748b8fbf413d3d51500
Stephanie Marsh <smarsh@joor.com>
2020-04-17 16:13:31 -0400

    fix entrypoint, chang to build folder

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react'
 
-const PhilPlaceholder = ({
-    text,
-    className,
-    imgHeight,
-    imgWidth,
-    altText="",
-  }) => {
-      const getImageSrc = () => `https://www.fillmurray.com/${imgWidth}/${imgHeight}`
+import './app.css'
 
-      return (
-        <div className={className} >
-          {text && <p>{text}</p>}
-          <img src={getImageSrc()} alt={altText}/>
-        </div>
-      )
+const PhilPlaceholder = ({
+  text,
+  className,
+  imgHeight,
+  imgWidth,
+  altText="",
+}) => {
+  const getImageSrc = () => `https://www.fillmurray.com/${imgWidth}/${imgHeight}`
+  console.log('heyyyy im phil', text, className, imgHeight)
+  return (
+    <div className={className} >
+      {text && <p>{text}</p>}
+      <img src={getImageSrc()} alt={altText}/>
+    </div>
+  )
 }
 
 export default PhilPlaceholder
