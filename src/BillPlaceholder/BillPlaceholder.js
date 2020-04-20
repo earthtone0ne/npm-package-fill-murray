18ef805f17fb7139b5384f8b0fde98f3c237ef26
Stephanie Marsh <smarsh@joor.com>
2020-04-20 19:20:28 -0400

    restructure to allow multiple components exported

---
+++ src/BillPlaceholder/BillPlaceholder.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// import './app.css'
+
+const BillPlaceholder = ({
+  text,
+  className,
+  imgHeight,
+  imgWidth,
+  altText="",
+}) => {
+  const getImageSrc = () => `https://www.fillmurray.com/${imgWidth}/${imgHeight}`
+  return (
+    <div className={className} >
+      {text && <p>{text.toUpperCase()}</p>}
+      <img src={getImageSrc()} alt={altText}/>
+    </div>
+  )
+}
+
+export default BillPlaceholder
--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import React from 'react'
-
-// import './app.css'
-
-const BillPlaceholder = ({
-  text,
-  className,
-  imgHeight,
-  imgWidth,
-  altText="",
-}) => {
-  const getImageSrc = () => `https://www.fillmurray.com/${imgWidth}/${imgHeight}`
-  return (
-    <div className={className} >
-      {text && <p>{text}</p>}
-      <img src={getImageSrc()} alt={altText}/>
-    </div>
-  )
+import BillPlaceholder from './BillPlaceholder/BillPlaceholder'
+console.log(BillPlaceholder)
+export default {
+  BillPlaceholder
 }
-
-export default BillPlaceholder
